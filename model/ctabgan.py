6153dfeb472cb48882eb9467a013169517ce3e39
unknown <josef.eiglsperger@web.de>
2022-01-27 12:03:58 +0100

    added selecting more hyperparameters

--- model/ctabgan.py
+++ model/ctabgan.py
@@ -21,11 +21,17 @@ class CTABGAN():
                  mixed_columns= {'capital-loss':[0.0],'capital-gain':[0.0]},
                  integer_columns = ['age', 'fnlwgt','capital-gain', 'capital-loss','hours-per-week'],
                  problem_type= {"Classification": 'income'},
-                 epochs = 1):
+                 epochs = 1,
+                 batch_size=500,
+                 class_dim=(256, 256, 256, 256),
+                 random_dim=100,
+                 num_channels=64,
+                 l2scale=1e-5):
 
         self.__name__ = 'CTABGAN'
               
-        self.synthesizer = CTABGANSynthesizer(epochs = epochs)
+        self.synthesizer = CTABGANSynthesizer(epochs = epochs, batch_size = batch_size, class_dim = class_dim, random_dim = random_dim,
+                                              num_channels = num_channels, l2scale = l2scale)
         self.raw_df = pd.read_csv(raw_csv_path)
         self.test_ratio = test_ratio
         self.categorical_columns = categorical_columns
@@ -33,20 +39,20 @@ class CTABGAN():
         self.mixed_columns = mixed_columns
         self.integer_columns = integer_columns
         self.problem_type = problem_type
-        
+
     def fit(self):
-        
+
         start_time = time.time()
         self.data_prep = DataPrep(self.raw_df,self.categorical_columns,self.log_columns,self.mixed_columns,self.integer_columns,self.problem_type,self.test_ratio)
-        self.synthesizer.fit(train_data=self.data_prep.df, categorical = self.data_prep.column_types["categorical"], 
+        self.synthesizer.fit(train_data=self.data_prep.df, categorical = self.data_prep.column_types["categorical"],
         mixed = self.data_prep.column_types["mixed"],type=self.problem_type)
         end_time = time.time()
         print('Finished training in',end_time-start_time," seconds.")
 
 
-    def generate_samples(self):
-        
-        sample = self.synthesizer.sample(len(self.raw_df)) 
+    def generate_samples(self, num_samples):
+
+        sample = self.synthesizer.sample(num_samples)
         sample_df = self.data_prep.inverse_prep(sample)
         
         return sample_df
